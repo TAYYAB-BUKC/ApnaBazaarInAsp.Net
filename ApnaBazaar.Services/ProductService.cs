92ddf5b79ac3c8c3f44fbbe74790e229910ce1f5
TAYYAB DOST <tayyabdost.bukc@gmail.com>
2020-01-26 17:56:21 +0500

    Successfully implemented Add/Search on a Product using Ajax

---
+++ ApnaBazaar.Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using ApnaBazaar.Database;
+using ApnaBazaar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApnaBazaar.Services
+{
+	public class ProductService
+	{
+		public Product GetSpecificProduct(int Id)
+		{
+			using (var context = new ApnaBazaarContext())
+			{
+				return context.Products.Find(Id);
+			}
+		}
+		public List<Product> GetProducts()
+		{
+			using (var context = new ApnaBazaarContext())
+			{
+				return context.Products.ToList();
+			}
+		}
+		public void SaveProduct(Product product)
+		{
+			using (var context = new ApnaBazaarContext())
+			{
+				context.Products.Add(product);
+				context.SaveChanges();
+			}
+		}
+
+		public void UpdateProduct(Product product)
+		{
+			using (var context = new ApnaBazaarContext())
+			{
+				context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+
+		public void DeleteCategory(int Id)
+		{
+			using (var context = new ApnaBazaarContext())
+			{
+				//context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+				var product = context.Products.Find(Id);
+				context.Products.Remove(product);
+				context.SaveChanges();
+			}
+		}
+	}
+}
---
+++ ApnaBazaar.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using ApnaBazaar.Entities;
+using ApnaBazaar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApnaBazaar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+		ProductService productService = new ProductService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult ProductTable(string search)
+		{
+			var products = productService.GetProducts();
+			if (!String.IsNullOrEmpty(search))
+			{
+			    products = products.Where(product => product.Name.Contains(search)).ToList();
+			}
+			return PartialView(products);
+		}
+
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Create(Product product)
+		{
+			productService.SaveProduct(product);
+			return RedirectToAction("ProductTable");
+		}
+
+	}
+}
